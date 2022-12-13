c97f7b613ca828a6354159321d0c1aeb4bf2e7ca
Samuel <achakarasamuel@gmail.com>
2022-12-13 15:58:04 -0800

    Task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,17 @@
  */
 int main(void)
 {
-	int n;
+	int n = 0;
 
-	for (n = 0; n < 10; n++)
-		putchar(n);
-	if (n < 9)
+	while (n < 10)
 	{
-		putchar(',');
-		putchar(' ');
+		putchar(48 + n);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
 	}
 
 	putchar('\n');
