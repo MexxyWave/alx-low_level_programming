4f039425ba2077b77789aa7efb053a165b4a8e5a
Samuel <achakarasamuel@gmail.com>
2022-12-16 09:51:22 -0800

    Task0

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - tests functions that print if the integer is positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
