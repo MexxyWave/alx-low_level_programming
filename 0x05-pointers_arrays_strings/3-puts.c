0ea63bd8da6d785cebb8af034080ac9960d89399
Samuel <achakarasamuel@gmail.com>
2022-12-20 14:48:21 -0800

    Task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - put a string
+ * @str: string parameter
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		++str;
+	}
+	_putchar('\n');
+}
