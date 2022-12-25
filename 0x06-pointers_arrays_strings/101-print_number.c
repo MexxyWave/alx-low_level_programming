e6f56552f2004fb40a8ebf304f2355965b99df77
Samuel <achakarasamuel@gmail.com>
2022-12-24 22:34:21 -0800

    Task9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,11 +14,15 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
 		n1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n1 = n;
 	}
 
-	if (n1 / 10 != 0)
+	if (n1 / 10)
 	{
 		print_number(n1 / 10);
 	}
