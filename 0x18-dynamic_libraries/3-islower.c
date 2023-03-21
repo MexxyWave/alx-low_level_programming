79b3dd83d5fa9987eca9ffea4adea372b5f653fd
Samuel <achakarasamuel@gmail.com>
2023-03-21 01:17:58 -0700

    Tasks update

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - the character
+ * @c: the character to be checked
+ * Return: 1 if letter is lowercase, otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
